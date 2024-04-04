b3fa7d09c0e55e38b5593e8c9720a15854001765
authentid dev (Reza) <authentiddev@outlook.com>
2024-04-04 12:21:37 +0100

    reztest git

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,6 +11,7 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
+				//reztest
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
