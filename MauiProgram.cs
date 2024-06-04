f334d96f61c27ccef320aefe3f45d0eef3208e7e
Reza Ataeigharacheh <bytebridgedev@outlook.com>
2024-06-04 21:30:29 +0100

    Intial microcharts setup with a test RadialGaugeChart

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using HabitGuiderMobileApp.Data;
 using HabitGuiderMobileApp.ViewModels;
 using HabitGuiderMobileApp.Views;
+using Microcharts.Maui;
 using Microsoft.Extensions.Logging;
 
 namespace HabitGuiderMobileApp;
@@ -12,6 +13,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMicrocharts()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,14 +1,50 @@
 using HabitGuiderMobileApp.ViewModels;
+using Microcharts;
+using SkiaSharp;
 
 namespace HabitGuiderMobileApp.Views;
 
 public partial class HomeView : ContentPage
 {
-	private readonly HomeViewModel _viewModel;
+    ChartEntry[] entries = new[]
+        {
+            new ChartEntry(212)
+            {
+                Label = "Windows",
+                ValueLabel = "112",
+                Color = SKColor.Parse("#2c3e50")
+            },
+            new ChartEntry(248)
+            {
+                Label = "Android",
+                ValueLabel = "648",
+                Color = SKColor.Parse("#77d065")
+            },
+            new ChartEntry(128)
+            {
+                Label = "iOS",
+                ValueLabel = "428",
+                Color = SKColor.Parse("#b455b6")
+            },
+            new ChartEntry(514)
+            {
+                Label = ".NET MAUI",
+                ValueLabel = "214",
+                Color = SKColor.Parse("#3498db")
+            }
+        };
+    private readonly HomeViewModel _viewModel;
 	public HomeView(HomeViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
 		_viewModel = vm;
-	}
+
+        chartView.Chart = new RadialGaugeChart
+        {
+            Entries = entries
+        };
+
+
+    }
 }
